a3894740b69de8536e6e728fca94c1b5c02b77e3
salih <salihozturk020@gmail.com>
2020-11-11 16:03:26 +0100

    solved Problem5.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-//      Problem2 p2 = new Problem2();
-//      System.out.println(p2.reverse(1534236469));
-//      Problem3 p3 = new Problem3();
-//      System.out.println(p3.isPalindrome(121));
-        Problem4 p4 = new Problem4();
-        System.out.println(p4.romanToInt("D"));
-
+//        Problem2 p2 = new Problem2();
+//        System.out.println(p2.reverse(1534236469));
+//        Problem3 p3 = new Problem3();
+//        System.out.println(p3.isPalindrome(121));
+//        Problem4 p4 = new Problem4();
+//        System.out.println(p4.romanToInt("D"));
+        Problem5 p5 = new Problem5();
+        String[] array = {"flower" , "fly","flight" , "flexible"};
+        System.out.println(p5.longestCommonPrefix(array));
 
     }
 }
---
+++ src/Problem5.java
@@ -0,0 +1,26 @@
+//Longest Common Prefix
+
+
+//Write a function to find the longest common prefix string amongst an array of strings.
+//
+//If there is no common prefix, return an empty string "".
+
+//example
+//Input: strs = ["flower","flow","flight"]
+//Output: "fl"
+
+public class Problem5 {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs == null || strs.length ==0)  return "";
+
+        String common = strs[0];
+
+        for(int i = 1 ; i<strs.length; i++){
+
+            for(;strs[i].indexOf(common) != 0 ;){
+
+                common = common.substring(0,common.length()-1);
+            }
+        }
+        return common;
+}}
